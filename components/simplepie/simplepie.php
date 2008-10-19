eaa9926dae677d69c75cc29f31d4b8a005f916f7
unknown <matt@mcurry.net>
2008-10-19 17:20:43 -0400

    Updated to CakePHP 1.2 RC3

--- components/simplepie/simplepie.php
+++ components/simplepie/simplepie.php
@@ -1,13 +1,14 @@
 <?php
 /*
  * SimplePie CakePHP Component
- * Copyright (c) 2007 Matt Curry
+ * Copyright (c) 2008 Matt Curry
  * www.PseudoCoder.com
+ * http://github.com/mcurry/cakephp/tree/master/components/simplepie
+ * http://sandbox2.pseudocoder.com/demo/simplepie
  *
  * Based on the work of Scott Sansoni (http://cakeforge.org/snippet/detail.php?type=snippet&id=53)
  *
- * @author      mattc <matt@pseudocoder.com>
- * @version     1.0
+ * @author      Matt Curry <matt@pseudocoder.com>
  * @license     MIT
  *
  */
@@ -23,12 +24,11 @@ class SimplepieComponent extends Object {
     
     //make the cache dir if it doesn't exist
     if (!file_exists($this->cache)) {
-      $folder = new Folder();
-      $folder->mkdirr($this->cache); 
+      $folder = new Folder($this->cache, true);
     }
 
     //include the vendor class
-    vendor('simplepie/simplepie');
+    App::import('vendor', 'simplepie');
 
     //setup SimplePie
     $feed = new SimplePie();
