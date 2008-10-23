5754f669af2f82e9a5ee312f23dc99ca185cf8c3
unknown <matt@mcurry.net>
2008-10-22 23:01:35 -0400

    Update for RC3. Now using jQuery.

--- plugins/chat/views/helpers/ajax_chat.php
+++ plugins/chat/views/helpers/ajax_chat.php
@@ -4,53 +4,37 @@ if (!defined('CHAT_FREQUENCY')) {
 }
 
 class AjaxChatHelper extends Helper {
-	var $helpers = array('Html', 'Javascript', 'Ajax', 'Form');
+	var $helpers = array('Html', 'Javascript', 'Form');
 
 	/*
 	key is an identifier for the chat.
 	You can have multiple chats by using different keys.
 	*/
 	function generate($key) {
-		$js = '';
-		$js .= 'function add_msg() {';
-		$js .= 'if (document.getElementById("ChatText").value != "") {';
-		$js .= 'var msgContainer = document.createElement("p");';
-		$js .= 'msgContainer.setAttribute("class", "new");';
-		$js .= 'var nameContainer = document.createElement("strong");';
+    $id = sprintf('chat_%s', $key);
 
+    echo $this->Javascript->codeBlock('
+          $(document).ready(function(){
+            $("#' . $id . '").chat();
+          });
+    ');    
+    
+		$html = array();
+		$html[] = '<div id="' . $id. '" class="chat" name="' . $key . '">';
+		$html[] = '<div class="chat_window"><p>Loading...</p></div>';
+		$html[] = $this->Form->create('Chat', array('id' => $key . 'ChatForm',
+                                                'url' => array('controller' => 'chat', 'action' => 'post')));
+		$html[] = $this->Form->input('key', array('type' => 'hidden', 'value' => $key));
+  
+    $html[] = $this->Form->input('name', array('id' => $key . 'ChatName'));
+    $html[] = $this->Form->input('message', array('id' => $key . 'ChatMessage',
+                                                  'type' => 'textarea'));
+    
+  	$html[] = $this->Form->end(__('Send', true));
+		$html[] = '</form>';
+		$html[] = '</div>';
 
-		$js .= 'var nameNode = document.createTextNode(document.getElementById("ChatHandle").value + " (now): ");';
-		$js .= 'var msgNode = document.createTextNode(document.getElementById("ChatText").value);';
-
-		$js .= 'nameContainer.appendChild(nameNode);';
-		$js .= 'msgContainer.appendChild(nameContainer);';
-		$js .= 'msgContainer.appendChild(msgNode);';
-		$js .= 'document.getElementById("chat_' . $key . '").insertBefore(msgContainer, document.getElementById("chat_' . $key . '").firstChild);';
-		$js .= '}';
-		$js .= '}';
-
-		$output = '';
-
-		$output .= '<div class="chat">';
-
-		//setup the timer that will update the chat window
-		$output .= $this->Ajax->remoteTimer(array('frequency' => CHAT_FREQUENCY, 'url' => '/chat/update/' . $key, 'update' => 'chat_' . $key));
-
-		$output .= '<div class="chat_window" id="chat_' . $key . '"><p>Loading...</p></div>';
-
-		$output .= $this->Ajax->form('/chat/post/' . $key, null, array('before' => 'add_msg()', 'after' => 'document.getElementById("ChatText").value=""'));
-		$output .= $this->Form->hidden('Chat/key', array('value' => $key));
-		$output .= '<label for="ChatHandle">Name:</label>' . $this->Form->input('Chat/handle', array('size' => '20', 'maxlength' => 20));
-		$output .= '<label for="ChatText">Message:</label>' .  $this->Form->input('Chat/text', array('size' => '45', 'maxlength' => 100));
-		$output .= $this->Form->submit('Send');
-		$output .= '</form>';
-		$output .= '</div>';
-
-		$output .= $this->Javascript->codeBlock($js);
-		//initial chat window update
-		$output .= $this->Javascript->codeBlock($this->Ajax->remoteFunction(array('url' => '/chat/init/' . $key, 'update' => 'chat_' . $key)));
-
-		return $output;
+		return "\n" . implode("\n", $html);
 	}
 }
 ?>
