8a8eb3c90f7f486dccc640938396080bcd15c753
unknown <matt@mcurry.net>
2008-10-22 22:56:34 -0400

    Update for RC3.  Now using jQuery.

---
+++ plugins/chat/vendors/js/chat.js
@@ -0,0 +1,58 @@
+(function($) {
+  var opts = {};
+
+  $.fn.chat = function(options) {
+    opts = $.extend({}, $.fn.chat.defaults, options);
+  
+    return this.each(function() {
+      var $this = $(this);
+      update($this);
+      setInterval(function() { update($this); }, opts.interval);
+      $(this).find("form").bind('submit', function() { post($(this)); return false});
+    });
+  };
+
+  function update($obj) {
+    $.ajax({
+      url: opts.update + "/" + $obj.attr("name"),
+      success: function(ret) {
+        $obj.find(".chat_window").html(ret);
+      }
+    });
+  };
+  
+  function post($obj) {
+    var $name =  $obj.find("input[name='data[Chat][name]']");
+    var $message =  $obj.find("textarea[name='data[Chat][message]']");
+    var $submit = $obj.find("input[type='submit']");
+    
+    if( ($.trim($name.val()) == "") || ($.trim($message.val()) == "") ) {
+      return;
+    }
+    
+    var form = $obj.serialize();
+    $message.attr('disabled', true);
+    $submit.attr('disabled', true);  
+
+    $.ajax({
+      type: "POST",
+      url: $obj.attr("action"),
+      data: form,
+      success: function() {
+        $message.val("");
+      },
+      complete: function() {
+        $message.attr('disabled', false);
+        $submit.attr('disabled', false);
+      }
+    });
+  };
+
+  //
+  // plugin defaults
+  //
+  $.fn.chat.defaults = {
+    update: '/chat/update',
+    interval: 5000
+  };
+})(jQuery);
