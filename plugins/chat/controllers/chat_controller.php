e04b43a1ef1227210ed56f20f627f183b207a007
unknown <matt@mcurry.net>
2008-10-22 23:13:46 -0400

    Updated file headers

--- plugins/chat/controllers/chat_controller.php
+++ plugins/chat/controllers/chat_controller.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * CakePHP Ajax Chat Plugin (using jQuery);
+ * Copyright (c) 2008 Matt Curry
+ * www.PseudoCoder.com
+ * http://github.com/mcurry/cakephp/tree/master/plugins/chat
+ * http://sandbox2.pseudocoder.com/demo/chat
+ *
+ * @author      Matt Curry <matt@pseudocoder.com>
+ * @license     MIT
+ *
+ */
 
 class ChatController extends ChatAppController {
 	var $name = 'Chat';
