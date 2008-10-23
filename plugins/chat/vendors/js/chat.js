6c34dd6b6b76c15fea2c42d13d4b928ebf7b67a3
unknown <matt@mcurry.net>
2008-10-22 23:13:46 -0400

    Updated file headers

--- plugins/chat/vendors/js/chat.js
+++ plugins/chat/vendors/js/chat.js
@@ -1,3 +1,15 @@
+/*
+ * CakePHP Ajax Chat Plugin (using jQuery);
+ * Copyright (c) 2008 Matt Curry
+ * www.PseudoCoder.com
+ * http://github.com/mcurry/cakephp/tree/master/plugins/chat
+ * http://sandbox2.pseudocoder.com/demo/chat
+ *
+ * @author      Matt Curry <matt@pseudocoder.com>
+ * @license     MIT
+ *
+ */
+
 (function($) {
   var opts = {};
 
