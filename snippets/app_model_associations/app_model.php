15c1c5a566b581876770abfbdb323c9c34d9d01a
unknown <matt@mcurry.net>
2009-04-17 11:18:47 -0400

    updated readme

--- snippets/app_model_associations/app_model.php
+++ snippets/app_model_associations/app_model.php
@@ -1,8 +1,8 @@
 <?php
 /*
- * App Model
+ * Unchained App Model
  * Copyright (c) 2009 Matt Curry
- * www.PseudoCoder.com
+ * http://www.pseudocoder.com/archives/2009/04/17/on-the-fly-model-chains-with-cakephp/
  *
  * @author      Matt Curry <matt@pseudocoder.com>
  * @license     MIT
